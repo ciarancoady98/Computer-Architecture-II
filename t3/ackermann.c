36aecfbe6d377449dc1ff395f14f18d6e7e0791b
ciaran <ccoady@tcd.ie>
2019-10-27 17:16:37 +0000

    initial tracking of overflows and underflows with a risc-I architecture and 6 register windows

--- t3/ackermann.c
+++ t3/ackermann.c
@@ -9,6 +9,7 @@ int SWP = 0;
 void overflowCheck(){
     if(WUSED == NWINDOWS){
         //overflowTrapHandler();
+        printf("an overflow has occured");
         SWP++;
     }
     else {
@@ -22,6 +23,7 @@ void underflowCheck(){
     if(WUSED == 2){
         SWP--;
         //underflowTrapHandler();
+        printf("an underflow has occured");
     }
     else {
         WUSED--;
@@ -32,19 +34,32 @@ void underflowCheck(){
 
 int ackermann(int x, int y){
     if(x==0){
+        underflowCheck();
         return y+1;
     }
     else if(y==0){
-        return ackermann(x-1, 1);
+        overflowCheck();
+        int result = ackermann(x-1, 1);
+        underflowCheck();
+        return result;
     }
     else {
-        return ackermann(x-1, ackermann(x,y-1));
+        overflowCheck();
+        int result1 = ackermann(x,y-1);
+        overflowCheck();
+        int result2 = ackermann(x-1, result1);
+        underflowCheck();
+        return result2;
     }
 }
 
+//Driver program
 int main( int argc, const char* argv[] ) {
-    //Driver program
-
+    //Allocating a register set for the main program
+    WUSED = 1;
+    //Test ackermann(3,6) with 6 register sets
+    NWINDOWS = 6;
+    overflowCheck();
     int result = ackermann(3, 6);
     printf("The result is %d",result);
 }
