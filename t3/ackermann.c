de7ab54be8e16a10498496285660aaded2788518
ciaran <ccoady@tcd.ie>
2019-10-27 17:06:42 +0000

    added functions to check overflow and underflow conditions

--- t3/ackermann.c
+++ t3/ackermann.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+int WUSED = 0;
+int NWINDOWS = 0;
+int CWP = 0;
+int SWP = 0;
+
+//Called before a function call
+void overflowCheck(){
+    if(WUSED == NWINDOWS){
+        //overflowTrapHandler();
+        SWP++;
+    }
+    else {
+        WUSED++;
+    }
+    CWP++;
+}
+
+//Called before a function return
+void underflowCheck(){
+    if(WUSED == 2){
+        SWP--;
+        //underflowTrapHandler();
+    }
+    else {
+        WUSED--;
+    }
+    CWP--;
+}
+
+
 int ackermann(int x, int y){
     if(x==0){
         return y+1;
@@ -13,7 +43,8 @@ int ackermann(int x, int y){
 }
 
 int main( int argc, const char* argv[] ) {
-    //Driver function
+    //Driver program
+
     int result = ackermann(3, 6);
     printf("The result is %d",result);
 }
