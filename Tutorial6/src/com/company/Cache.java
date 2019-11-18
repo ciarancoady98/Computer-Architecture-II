8a12513e7c4fc93f6dfde59003e7779223dddcbe
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-18 17:49:42 +0000

    created a skeleton class to represent a cache

--- Tutorial6/src/com/company/Cache.java
+++ Tutorial6/src/com/company/Cache.java
@@ -1,4 +1,37 @@
 package com.company;
 
 public class Cache {
+    /*
+    This object represents a cache where:
+    L = the line size of the cache
+    N = the number of sets (rows) in the cache
+    K = how many tags there are per cache line (k-way associative)
+     */
+
+    int L;
+    int N;
+    int K;
+    int[][] cache;
+
+    Cache(int L, int N, int K){
+        this.L = L;
+        this.N = N;
+        this.K = K;
+        this.cache = new int[N][K+L];
+    }
+
+    /*
+    Takes a memory address and maps it onto a set
+     */
+    int map(){
+        return -1;
+    }
+
+    void insert(){
+
+    }
+
+    void evict(){
+
+    }
 }
