41a433f53b1bee20e11e8a70a0979d4b13408d81
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-11-18 23:00:30 +0000

    Working solution tested with 128 byte 1-way cache

--- t6/src/com/company/Cache.java
+++ t6/src/com/company/Cache.java
@@ -2,36 +2,35 @@ package com.company;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Cache {
-    /*
-    This object represents a cache where:
-    L = the line size of the cache
-    N = the number of sets (rows) in the cache
-    K = how many tags there are per cache line (k-way associative)
-     */
 
-    int L;
-    int N;
-    int K;
-    int offsetBits;
-    int numberOfSetsBits;
-    int offsetMask;
-    int setMask;
-    HashMap[] cache;
+    int L; //the line size of the cache
+    int N; //the number of sets (rows) in the cache
+    int K; //how many tags there are per cache line (k-way associative)
+    int offsetBits; //how many bits are being used as the offset
+    int numberOfSetsBits; //how many bits are being used to identify sets
+    int offsetMask; //the mask used to extract the offset into the cache line
+    int setMask; //the mask used to extract the set number from address
+    LinkedList[] cache; //datastructure used to store the cache
+    int numberOfHits;
+    int numberOfMisses;
 
     Cache(int L, int N, int K){
         this.L = L;
         this.N = N;
         this.K = K;
-        this.cache = new HashMap[N];
+        this.cache = new LinkedList[N];
         for(int i = 0; i < N; i++) {
-            this.cache[i] = new HashMap<Integer, Date>();
+            this.cache[i] = new LinkedList<Integer>();
         }
         this.offsetBits = (int) (Math.log(L) / Math.log(2));
         this.numberOfSetsBits = (int) (Math.log(N) / Math.log(2));
         this.offsetMask = getMask(0, offsetBits);
         this.setMask = getMask(offsetBits, (numberOfSetsBits+offsetBits));
+        this.numberOfHits = 0;
+        this.numberOfMisses = 0;
     }
 
     /*
@@ -54,36 +53,40 @@ public class Cache {
     Takes a memory address and if it is in the cache returns its value
     otherwise it returns -1 to signify a miss
      */
-    int getValue(int address){
+    boolean inCache(int address){
         //Addresses are 16bit hex representations
         int offset = address & this.offsetMask;
         int setNumber = (address & this.setMask) >> this.offsetBits;
-        System.out.println("offset: " + offset + " setNumber: " + setNumber);
-        //bit shift offset out of addr
-        //bit shift setNumber out of addr
-        //result is addr in hash map as int
-        return -1;
-    }
-
-    /*
-    Takes a memory address and maps it onto a set
-     */
-    int mapToSet(){
-        return -1;
+        int addressTag = address >>> (this.offsetBits+this.numberOfSetsBits);
+        System.out.println("addressTag: " + addressTag + " setNumber: " + setNumber + " offset: " + offset);
+        if(this.cache[setNumber].contains(addressTag)){
+            this.cache[setNumber].remove((Object)addressTag);
+            this.cache[setNumber].addFirst(addressTag);
+            this.numberOfHits++;
+            return true;
+        }
+        else {
+            insert(addressTag,setNumber);
+            this.numberOfMisses++;
+            return false;
+        }
     }
 
     /*
     Takes an address and a set and inserts that address
     in the set
      */
-    void insert(int tag){
-
+    void insert(int tag, int set){
+        if(this.cache[set].size() == this.K){
+            evict(set);
+        }
+        this.cache[set].addFirst(tag);
     }
 
     /*
     Removes the least recently used tag from a specific set
      */
     void evict(int set){
-
+        this.cache[set].removeLast();
     }
 }
--- t6/src/com/company/Main.java
+++ t6/src/com/company/Main.java
@@ -5,8 +5,38 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Cache myCache = new Cache(16,8,1);
-        myCache.getValue(0x0000);
-        myCache.getValue(0x0004);
-        myCache.getValue(0x0028);
+        myCache.inCache(0x0000);
+        myCache.inCache(0x0004);
+        myCache.inCache(0x000C);
+        myCache.inCache(0x2200);
+        myCache.inCache(0x00D0);
+        myCache.inCache(0x00E0);
+        myCache.inCache(0x1130);
+        myCache.inCache(0x0028);
+        myCache.inCache(0x113C);
+        myCache.inCache(0x2204);
+        myCache.inCache(0x0010);
+        myCache.inCache(0x0020);
+        myCache.inCache(0x0004);
+        myCache.inCache(0x0040);
+        myCache.inCache(0x2208);
+        myCache.inCache(0x0008);
+        myCache.inCache(0x00A0);
+        myCache.inCache(0x0004);
+        myCache.inCache(0x1104);
+        myCache.inCache(0x0028);
+        myCache.inCache(0x000C);
+        myCache.inCache(0x0084);
+        myCache.inCache(0x000C);
+        myCache.inCache(0x3390);
+        myCache.inCache(0x00B0);
+        myCache.inCache(0x1100);
+        myCache.inCache(0x0028);
+        myCache.inCache(0x0064);
+        myCache.inCache(0x0070);
+        myCache.inCache(0x00D0);
+        myCache.inCache(0x0008);
+        myCache.inCache(0x3394);
+        System.out.println("Number Of Misses: " + myCache.numberOfMisses + " Number of Hits: " + myCache.numberOfHits);
     }
 }
